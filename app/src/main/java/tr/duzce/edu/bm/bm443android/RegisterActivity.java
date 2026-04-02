82feecf7b630f2976dfd0bc5498add2bd84f979b
Fase2507 <fase2507@gmail.com>
2026-04-02 22:31:51 +0300

    autocompletion ve array eklendi

--- app/src/main/java/tr/duzce/edu/bm/bm443android/RegisterActivity.java
+++ app/src/main/java/tr/duzce/edu/bm/bm443android/RegisterActivity.java
@@ -2,6 +2,8 @@ package tr.duzce.edu.bm.bm443android;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,6 +18,8 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText input;
     private int sayac;
     private Button btnKaydet;
+
+    private AutoCompleteTextView txtBolum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +42,21 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void initComponents(){
+        txtBolum = findViewById(R.id.txtBolum);
         txt = findViewById(R.id.kayitFormu);
         input = findViewById(R.id.kayitText);
         btnKaydet = findViewById(R.id.kayitButon);
+        String[] bolumler = getResources().getStringArray(R.array.bolumler);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                bolumler
+        );
+
+        // 3. Set the adapter to the AutoCompleteTextView
+        txtBolum.setAdapter(adapter);
+
+        // Optional: Set threshold to 1 so suggestions appear after typing 1 character
+        txtBolum.setThreshold(1);
     }
 }
